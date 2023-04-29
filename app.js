6273a653cb90ca3416ab176db1ae891b335d2153
Emil Bayanov <atex-69@mail.ru>
2023-04-29 23:45:04 +0300

    update 13work

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '61e6af7a10fde19d8300a57b',
+  };
+
+  next();
+});
+
+app.use(routes);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
