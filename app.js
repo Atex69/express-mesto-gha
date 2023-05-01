8a42cba13956dd252d0407fc743e1829c5e79856
Emil Bayanov <atex-69@mail.ru>
2023-05-01 04:45:01 +0300

    fix 13work

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const helmet = require('helmet');
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -19,11 +19,9 @@ app.use(helmet());
 app.use(routes);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-
--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    },
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
   about: {
     type: String,
     required: [true, 'Поле "about" должно быть заполнено'],
